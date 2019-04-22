cbef383aa6d49fbfbdc16c90d57827cd68612228
FaydSpeare <fspea123@gmail.com>
2019-04-22 23:42:02 +1000

    class skeleton

---
+++ node.py
@@ -0,0 +1,37 @@
+class Node:
+
+    def __init__():
+        self.output_nodes = []
+        self.input_nodes = []
+
+class InputNode(Node):
+
+    def __init__():
+        print(":)")
+
+class OutputNode(Node):
+
+    def __init__():
+        print(":)")
+
+class HiddenNode(Node):
+
+    def __init__():
+        print(":)")
+
+class Connection:
+
+    def __init(input_node, output_node):
+        self.input = input_node
+        self.output = output_node
+        self.weight = 0
+
+class Network:
+
+    def __init__():
+        self.nodes = []
+
+    def feed_forward():
+        return 0
+        
+        
